c5fb4901da3e982cafda83fbf7acf5dc0787f06d
Ashwani Singh <ashwani.singh@opstree.com>
2023-07-23 09:46:58 +0530

    shared lib

---
+++ src/org/mytools/Tools.groovy
@@ -0,0 +1,11 @@
+package org.mytools
+class Tools implements Serializable {
+    private static final long serialVersionUID
+    def steps
+    Tools(steps) {
+        this.steps = steps
+    }
+    void myEcho(String msg) {
+        steps.echo msg
+    }
+}
